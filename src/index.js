a1693f4e20319775387e7a05d9d0f9483d708914
Kaitlyn Greve <kgreve14@gmail.com>
2022-04-12 16:48:44 -0400

    commit tree c2741075363d1e9e869029fde3610a97e549e4ff

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+const fetchDogs = () => {
+    fetch(imgUrl)
+        .then(response => response.json())
+        .then(data => addImage(data))
+        // .then(response => console.log(response.json))
+}
